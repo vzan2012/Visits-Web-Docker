c673e5e66466703f2412c14942edd8ad0eb12056
Deepak Guptha <vzan2012@gmail.com>
2020-04-05 13:53:37 +0530

    ci(docker): Updated the docker file for node and redis

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const redis = require("redis");
+
+const app = express();
+
+const client = redis.createClient({
+  host: "redis-server",
+  port: 6379,
+});
+
+const port = process.env.PORT || 3000;
+
+client.set("visits", 0);
+
+app.get("/", (req, res) => {
+  // process.exit(0);
+
+  client.get("visits", (err, visits) => {
+    res.send(`Number of visits is ${visits}`);
+    client.set("visits", parseInt(visits) + 1);
+  });
+});
+
+app.listen(port, () => console.log(`Listening on PORT: ${port}`));
